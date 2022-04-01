99e39d40a53a2606364bada1e9af892e7ce4db8b
Pedro <pedro.monteiro.ps@gmail.com>
2022-04-01 01:51:51 +0100

    remove comments

--- src/LaravelFingerprintsServiceProvider.php
+++ src/LaravelFingerprintsServiceProvider.php
@@ -11,36 +11,11 @@ class LaravelFingerprintsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-fingerprints');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-fingerprints');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('laravel-fingerprints.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-fingerprints'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-fingerprints'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-fingerprints'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
